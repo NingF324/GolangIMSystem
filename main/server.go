f05d686abb7f9ccf7387d002a90d3e49111cce9d
NingF324 <2283451158@qq.com>
2024-09-10 15:12:19 +0800

    initialize

---
+++ main/server.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"sync"
+)
+
+type Server struct {
+	Ip   string
+	Port int
+
+	OnlineMap map[string]*User
+	mapLock   sync.RWMutex
+
+	Message chan string
+}
+
+func NewServer(ip string, port int) *Server {
+	server := &Server{
+		Ip:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*User),
+		Message:   make(chan string),
+	}
+	return server
+}
+
+func (this *Server) ListenMessager() {
+	for {
+		msg := <-this.Message
+		this.mapLock.Lock()
+		for _, cli := range this.OnlineMap {
+			cli.C <- msg
+		}
+		this.mapLock.Unlock()
+	}
+}
+
+func (this *Server) BroadCast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+
+	this.Message <- sendMsg
+}
+
+func (this *Server) Handler(conn net.Conn) {
+	user := NewUser(conn, this)
+
+	user.Online()
+
+	go func() {
+		buf := make([]byte, 4096)
+		for {
+			n, err := conn.Read(buf)
+			if n == 0 {
+				user.Offline()
+				return
+			}
+
+			if err != nil && err != io.EOF {
+				fmt.Println("Conn Read err:", err)
+				return
+			}
+
+			msg := string(buf[:n-1])
+
+			user.DoMessage(msg)
+		}
+	}()
+
+	select {}
+}
+
+func (this *Server) Start() {
+	//socket listen
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", this.Ip, this.Port))
+	//close listen socket
+	if err != nil {
+		fmt.Println("net.Listen err:", err)
+		return
+	}
+	defer listener.Close()
+	go this.ListenMessager()
+
+	for {
+		//accept
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("listener accept err:", err)
+			continue
+		}
+		//do handler
+
+		go this.Handler(conn)
+	}
+
+}
---
+++ main/user.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"net"
+	"strings"
+)
+
+type User struct {
+	Name string
+	Addr string
+	C    chan string
+	conn net.Conn
+
+	server *Server
+}
+
+func NewUser(conn net.Conn, server *Server) *User {
+	userAddr := conn.RemoteAddr().String()
+	user := &User{
+		Name:   userAddr,
+		Addr:   userAddr,
+		C:      make(chan string),
+		conn:   conn,
+		server: server,
+	}
+	go user.ListenMessage()
+
+	return user
+}
+
+func (this *User) ListenMessage() {
+	for {
+		msg := <-this.C
+
+		this.conn.Write([]byte(msg + "\n"))
+
+	}
+}
+
+func (this *User) Online() {
+	this.server.mapLock.Lock()
+	this.server.OnlineMap[this.Name] = this
+	this.server.mapLock.Unlock()
+
+	this.server.BroadCast(this, "已上线")
+}
+
+func (this *User) Offline() {
+	this.server.mapLock.Lock()
+	delete(this.server.OnlineMap, this.Name)
+	this.server.mapLock.Unlock()
+
+	this.server.BroadCast(this, "下线")
+}
+
+func (this *User) SendMsg(msg string) {
+	this.conn.Write([]byte(msg))
+}
+
+func (this *User) DoMessage(msg string) {
+	if msg == "who" {
+		this.server.mapLock.Lock()
+		for _, user := range this.server.OnlineMap {
+			onlineMsg := "[" + user.Addr + "]" + user.Name + ":" + "在线...\n"
+			this.SendMsg(onlineMsg)
+
+		}
+		this.server.mapLock.Unlock()
+	} else if len(msg) > 7 && msg[:7] == "rename|" {
+		newName := strings.Split(msg, "|")[1]
+		_, ok := this.server.OnlineMap[newName]
+		if ok {
+			this.SendMsg("当前用户名被使用\n")
+		} else {
+			this.server.mapLock.Lock()
+			delete(this.server.OnlineMap, this.Name)
+			this.server.OnlineMap[newName] = this
+			this.server.mapLock.Unlock()
+			this.Name = newName
+			this.SendMsg("您已经更新用户名：" + this.Name + "\n")
+		}
+	} else {
+		this.server.BroadCast(this, msg)
+	}
+}
